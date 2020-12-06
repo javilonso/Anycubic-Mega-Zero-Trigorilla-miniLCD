3ecb11d26f0d70a1d5679f958d608404a949acb4
Javi <jmaker3d@gmail.com>
2020-12-06 21:37:09 +0100

    all working, bug in fans trigorilla pins 14

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -725,7 +725,8 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {157, 157, 800, 819.14 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {163.3, 163.3, 804, 819.14 } //TMC2225 Javi
+//#define DEFAULT_AXIS_STEPS_PER_UNIT   {80, 80, 400, 380 } //a4988
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -745,7 +746,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 500, 500, 100, 5000 }
+#define DEFAULT_MAX_ACCELERATION      { 2000, 2000, 100, 5000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
--- Marlin/src/pins/ramps/pins_TRIGORILLA_14.h
+++ Marlin/src/pins/ramps/pins_TRIGORILLA_14.h
@@ -51,8 +51,8 @@
 #define TRIGORILLA_HEATER_0_PIN               10
 #define TRIGORILLA_HEATER_1_PIN               45  // Anycubic Kossel: Unused
 
-#define TRIGORILLA_FAN0_PIN                    9  // Anycubic Kossel: Usually the part cooling fan
-#define TRIGORILLA_FAN1_PIN                  44  // Anycubic Kossel: Unused
+#define TRIGORILLA_FAN0_PIN                    -1  // Anycubic Kossel: Usually the part cooling fan
+#define TRIGORILLA_FAN1_PIN                  9  // Blower Fan Javi
 #define TRIGORILLA_FAN2_PIN                   -1  // Anycubic Kossel: Hotend fan
 
 // Remap MOSFET pins to common usages:
